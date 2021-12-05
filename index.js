6c704c0a8146e4711b04f71cdc7dce2b44b2bdb1
Jared <jbeckersalax9@aol.com>
2021-12-04 23:29:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your solution in this file!
+const employee = {
+    name: 'nick',
+    streetAddress: '1054 drink avenue'
+};
+
+function updateEmployeeWithKeyAndValue(){
+    return{ ...employee,
+        name: 'Sam',
+        streetAddress: '11 Broadway'
+    };
+};
+
+destructivelyUpdateEmployeeWithKeyAndValue = employee.name = 'Sam'
+
+destructivelyUpdateEmployeeWithKeyAndValue = employee.streetAddress = '12 Broadway'
